dea5771883f9539497728403ef97063bc0ada356
Alfonso Cuffaro <a.cuffaro@gmail.com>
2025-02-25 12:53:39 +0100

    add java sources generated by spring initializer

---
+++ src/main/java/com/example/beorders/BeordersApplication.java
@@ -0,0 +1,13 @@
+package com.example.beorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeordersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeordersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/beorders/BeordersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeordersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
