2b5f1ff1b0aeaf42a0bcddaf8460ae3c133b4118
Alfonso Cuffaro <a.cuffaro@gmail.com>
2025-03-02 23:50:05 +0100

    remove Product.java because is not used anymore

--- src/main/java/com/example/beorders/orders/Product.java
+++
@@ -1,8 +0,0 @@
-package com.example.beorders.orders;
-
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
-
-@Table(name = "BE_PRODUCT")
-public record Product(@Id Long id, String name, Double price) {
-}
